19ae8856ddcacefec895331a36c678302e310de4
Chris Lovering <chris.lovering.95@gmail.com>
2021-11-13 22:57:02 +0000

    Add channel_id and indexes to messages table

--- patsy/models/messages.py
+++ patsy/models/messages.py
@@ -4,8 +4,6 @@ from sqlmodel import BigInteger, Column, Field, ForeignKey, SQLModel
 class MessageBase(SQLModel):
     """A base model for storing information about messages."""
 
-    author_id: int
-    session_id: int
     content: str
 
 
@@ -34,7 +32,32 @@ class MessagesTable(MessageBase, table=True):
             "session_id",
             BigInteger,
             ForeignKey("help_sessions.session_id"),
-            nullable=False
+            nullable=False,
+            index=True
+        )
+    )
+    channel_id: int = Field(
+        sa_column=Column(
+            "channel_id",
+            BigInteger,
+            nullable=True,
+            index=True
         )
     )
     content: str = Field(nullable=False, max_length=4000)
+
+
+class MessagePost(MessageBase):
+    """Information required to create a message."""
+
+    message_id: int
+    author_id: int
+    channel_id: int
+
+
+class MessageUpdate(MessageBase):
+    """Information required to update a message."""
+
+
+class MessageRead(MessagesTable):
+    """Information about a single message."""
